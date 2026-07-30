be6db78d985c6ac294684f4fb9050af0c237c58f
code-developer <jayantjakhar2006@gmail.com>
2026-07-30 09:35:25 +0530

    My-react-app

---
+++ Day/Day12/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { HelloWorld } from "./components/HelloWorld";
+function App() {
+  
+  return (
+      <div>
+        <h1>Hello, World!</h1>
+        <h3>Welcome to my first React app.</h3>
+      </div>
+  )
+}
+
+export default App
